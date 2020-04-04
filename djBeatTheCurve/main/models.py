ba1f673e8d9500ec2f1df9bc5e7a8a257d15ac84
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-04-04 13:31:15 +0200

    Add models

--- djBeatTheCurve/main/models.py
+++ djBeatTheCurve/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Commorbidity(models.Model):
+    name = models.CharField(max_length=200)
+
+class CustomUser(models.Model):
+
+    class Gender(models.TextChoices):
+        FEMALE = 'F', _('Female')
+        MALE = 'M', _('Male')
+        OTHER = 'O', _('Other')
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(
+        max_length=2,
+        choices=Gender.choices,
+        default=Gender.OTHER,
+    )
+    household_size = models.PositiveIntegerField()
+    commorbidities = models.ForeignKey(Commorbidity, on_delete=models.DO_NOTHING)
+
+class GameInfo(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    last_leave = models.DateTimeField('last household leave')
+    last_hand_wash = models.DateTimeField('last hand wash')
+    last_physical_activity = models.DateTimeField('last physical activity')
+    score = models.IntegerField()
