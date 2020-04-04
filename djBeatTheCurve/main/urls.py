508afc32848b5cc5122ad421b53ee7148b8524af
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-04-04 23:37:30 +0200

    Add simple html files for testing purpose with url path

--- djBeatTheCurve/main/urls.py
+++ djBeatTheCurve/main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('signup', views.signUp, name="signup"),
+    path('signin', views.signIn, name="signin"),
+    path('customsignup', views.customSignUp, name="customsignup"),
+    path('dashboard', views.dashboard, name="dashboard")
 ]
