932dc320ffb79f6f9dbe9577cae54be9e58f1d07
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-04-04 23:34:48 +0200

    Add new models to admin panel

--- djBeatTheCurve/main/admin.py
+++ djBeatTheCurve/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Commorbidity
+from .models import Symptom
+from .models import CustomUser
+
 # Register your models here.
+
+admin.site.register(Commorbidity)
+admin.site.register(Symptom)
+admin.site.register(CustomUser)
