694e395cdd6b041e0c7d42a1a30446292a4c9a16
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-04-04 14:26:57 +0200

    Add string method for debugging purposes

--- djBeatTheCurve/main/models.py
+++ djBeatTheCurve/main/models.py
@@ -6,6 +6,12 @@ from django.utils.translation import gettext_lazy as _
 
 class Commorbidity(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"""Commorbidity [
+            Name: {self.name}
+        ]
+        """
 
 class CustomUser(models.Model):
 
@@ -21,8 +27,18 @@ class CustomUser(models.Model):
         choices=Gender.choices,
         default=Gender.OTHER,
     )
-    household_size = models.PositiveIntegerField()
-    commorbidities = models.ForeignKey(Commorbidity, on_delete=models.DO_NOTHING)
+    household_size = models.PositiveIntegerField(default=1)
+    commorbidities = models.ManyToManyField(Commorbidity)
+
+    def __str__(self):
+        return f"""CustomUser [
+            Username: {self.user!s},
+            Age: {self.age},
+            Gender: {self.gender},
+            Household size: {self.household_size},
+            Commorbidities: {self.commorbidities}
+        ]
+        """
 
 class GameInfo(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
@@ -30,3 +46,13 @@ class GameInfo(models.Model):
     last_hand_wash = models.DateTimeField('last hand wash')
     last_physical_activity = models.DateTimeField('last physical activity')
     score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"""GameInfo [
+            Username: {self.user!s},
+            Date of last household leave: {self.last_leave},
+            Date of last hand wash: {self.last_hand_wash},
+            Date of last physical activity: {self.last_physical_activity},
+            Score: {self.score}
+        ]
+        """
