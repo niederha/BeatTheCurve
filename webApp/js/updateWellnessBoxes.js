5ce7695c1fa45cd652858aa157d9e699995b6b23
Loïc Niederhauser <loic.niederhauser@gmail.com>
2020-04-04 17:55:02 +0200

    First light addidition of js to update things slighlty more dynamically

---
+++ webApp/js/updateWellnessBoxes.js
@@ -0,0 +1,6 @@
+
+// Update figures 
+document.getElementById("nbHealthy").innerHTML = 1;
+document.getElementById("nbSick").innerHTML    = 2;
+document.getElementById("nbDead").innerHTML    = 3;
+document.getElementById("nbImmune").innerHTML  = 4;
