4a2c9c3b8c88a00267a9b4766765ac3a4b364ea6
Loïc Niederhauser <loic.niederhauser@gmail.com>
2020-04-06 00:54:00 +0200

    First prototype of working interactive graph

---
+++ webApp/js/bindCurveDisplayButtons.js
@@ -0,0 +1,65 @@
+// I wanted a class but this is js... so this is the next best thing
+function createDisplayCurveButtonHandler(buttonId, curveNum, color, isOnDisplay = true) {
+    var displayCurveButtonHandler = function(){};
+    displayCurveButtonHandler.button = document.getElementById(buttonId);
+    displayCurveButtonHandler.isOnDisplay = isOnDisplay;
+    displayCurveButtonHandler.textOncolor = "w3-text-white";
+    displayCurveButtonHandler.textOffColor = "w3-text-" + color;
+    displayCurveButtonHandler.buttonOnColor = "w3-" + color;
+    displayCurveButtonHandler.curveNum = curveNum;
+
+    // Setup borders if needed
+    var borderClassTag = "w3-border-" + color;
+    if (!displayCurveButtonHandler.button.classList.contains(borderClassTag)) {
+        displayCurveButtonHandler.button.classList.add(borderClassTag);
+    }
+    return displayCurveButtonHandler
+}
+function turnOnButton(bh){
+    console.log("on");
+    bh.button.classList.add(bh.buttonOnColor);
+    bh.button.classList.remove(bh.textOffColor);
+    bh.button.classList.add(bh.textOnColor);
+    drawChart.displayMask[bh.curveNum] = true;
+}
+function turnOffButton(bh) {
+    console.log("off");
+    bh.button.classList.remove(bh.buttonOnColor);
+    bh.button.classList.remove(bh.textOnColor);
+    bh.button.classList.add(bh.textOnColor);
+    drawChart.displayMask[bh.curveNum] = false;
+}
+function reverseButton(bh) {
+    if (bh.isOnDisplay) {
+        turnOffButton(bh);
+    } else {
+        turnOnButton(bh);
+    }
+    bh.isOnDisplay = !bh.isOnDisplay;
+    drawChart();
+}
+
+google.charts.setOnLoadCallback(setButtonCallbacks);
+
+// Button callback bindings
+
+
+
+function setButtonCallbacks() {
+    var buttonSuceptible = createDisplayCurveButtonHandler("buttonSuceptible", 0, "teal");
+    var buttonSick = createDisplayCurveButtonHandler("buttonSick", 1, "deep-orange");
+    var buttonDead = createDisplayCurveButtonHandler("buttonDead", 2, "blue-gray");
+    var buttonRecovered = createDisplayCurveButtonHandler("buttonRecovered", 3, "green");
+    var buttonAcuteSymptoms = createDisplayCurveButtonHandler("buttonAcuteSymptoms", 4, "dark-gray");
+    var buttonHospitalCapacity = createDisplayCurveButtonHandler("buttonHospitalCapacity", 5, "black");
+
+    buttonSuceptible.button.onclick = function(){reverseButton(buttonSuceptible)};
+    buttonSick.button.onclick = function () { reverseButton(buttonSick) };
+    buttonDead.button.onclick = function () { reverseButton(buttonDead) };
+    buttonRecovered.button.onclick = function () { reverseButton(buttonRecovered) };
+    buttonAcuteSymptoms.button.onclick = function () { reverseButton(buttonAcuteSymptoms) };
+    buttonHospitalCapacity.button.onclick = function () { reverseButton(buttonHospitalCapacity) };
+}
+
+
+
--- webApp/js/updateSimulationGraph.js
+++ webApp/js/updateSimulationGraph.js
@@ -1,17 +1,7 @@
-// Global variables
-var testButton;
-
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 window.onresize = drawChart;
-window.onload = loadAll;
-
-
-function loadAll(){
-    updateNumericalData();
-    testButton = new displayCurveButtonHandler("button1", "", "teal");
-    console.log(typeof testButton);
-}
+window.onload = updateNumericalData;
 
 function updateNumericalData(){
     document.getElementById("sim_nbSuceptible").innerHTML = 12345656;
@@ -22,14 +12,27 @@ function updateNumericalData(){
 }
 
 function drawChart() {
+
+    // Define static variables
+    if (typeof drawChart.displayMask == 'undefined'){
+        drawChart.displayMask = [true, true, true, true, true, true];
+    }
+
     var data = google.visualization.arrayToDataTable([
         ['Day since beginning', 'Suceptible',   'Infected', 'Deaths',   'Recovered', 'Hositalized', 'Hospital capacity'],
-        ['0',                   0,                 0,        0,          0,           0,                0],
+        ['0',                   0,                 0,        0,          0,           0,             1000],
         ['1',                   1000,            400,        0,          12,          44,            1000],
         ['2',                   900,             499,        20,         50,          100,           1000],
         ['3',                   300,             1100,       100,        300,         700,           1000],
         ['4',                   100,             2000,       500,        400,         1000,          1000]
     ]);
+    
+    view = new google.visualization.DataView(data);
+    for (var i = 0; i < drawChart.displayMask.length; i++){
+        if(!drawChart.displayMask[i]){
+            view.hideColumns([i+1]);
+        }
+    }
 
     var options = {
         title: '',
@@ -43,8 +46,7 @@ function drawChart() {
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('SimulationGraph'));
-
-    chart.draw(data, options);
+    chart.draw(view, options);
 }
 
 
