a8fd77aaa1ed0933e04a956d2eb295a323000e71
Niccolò <ssnnca@gmail.com>
2020-04-05 19:42:52 +0200

    maps and wellness boxes

--- webApp/js/updateInteractiveMap.js
+++ webApp/js/updateInteractiveMap.js
@@ -1,24 +1,64 @@
+/*jshint esversion: 6 */
 
-google.charts.load('current', {'packages': ['geochart']});
-google.charts.setOnLoadCallback(drawRegionsMap);
-window.onresize = drawRegionsMap;
+google.charts.load('current', {
+  'packages': ['geochart']
+});
+
+let dataPippo = [];
+
+function getPippoData() {
+  fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "815636d2f5msh5b6f6b41061625ep10f46fjsn4221a8a8f565"
+      }
+    })
+    .then(response => response.json().then(data => {
+			function fixElemName(el){
+				if (el.country_name == "USA")
+					el.country_name = "United States";
+				else if  (el.country_name == "UK")
+					el.country_name = "United Kingdom";
+			}
+      dataPippo = data.countries_stat;
+      dataPippo.forEach((el, i) => {
+				fixElemName(el);
+        dataPippo[i] = [el.country_name, el.cases];
+      });
+      dataPippo.unshift(['Country', 'Number cases']);
+    }))
+    .then(() => {
+      drawRegionsMap();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 function drawRegionsMap() {
 
-    var data = google.visualization.arrayToDataTable([
-        ['Country', 'Number cases'],
-        ['Germany', 200],
-        ['United States', 300],
-        ['Brazil', 400],
-        ['Canada', 500],
-        ['France', 600],
-        ['RU', 700]
-    ]);
+  var data = google.visualization.arrayToDataTable(
+    dataPippo
+  );
 
-    var options = {
-        backgroundColor: {fill:"transparent"},
-        colorAxis: {minValue: 0, maxValue: 1000, colors: ['green', 'red', 'black']},
-    };
-    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-    chart.draw(data, options);
+  var options = {
+    backgroundColor: {
+      fill: "transparent"
+    },
+    colorAxis: {
+      minValue: 0,
+      maxValue: 1000,
+      colors: ['green', 'red', 'black']
+    },
+  };
+  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+  chart.draw(data, options);
 }
+
+google.charts.setOnLoadCallback(getPippoData);
+window.onresize = drawRegionsMap;
--- webApp/js/updateWellnessBoxes.js
+++ webApp/js/updateWellnessBoxes.js
@@ -1,6 +1,40 @@
+/*jshint esversion: 6 */
+// Update figures
+//
 
-// Update figures 
-document.getElementById("nbSuceptible").innerHTML   = 12345656;
-document.getElementById("nbSick").innerHTML         = 1234789;
-document.getElementById("nbDead").innerHTML         = 324789017;
-document.getElementById("nbImmune").innerHTML       = 1723849;
+function divAddData(idElem, data1, data2){
+  container_element = document.getElementById(idElem);
+  container_element.innerHTML = "";
+  let div1 = document.createElement('div');
+  div1.classList.add("w3-container","w3-third","w3-margin", "w3-cell-middle");
+  let paragraph = document.createElement('h3');
+  paragraph.textContent = data1;
+  div1.appendChild(paragraph);
+  let div2 = document.createElement('div');
+  if (data2){
+    div2.classList.add("w3-container","w3-third","w3-margin", "w3-middle");
+    let paragraph2 = document.createElement('h7');
+    paragraph2.textContent = 'new: ' + data2;
+    div2.appendChild(paragraph2);
+  }
+  //paragraph.classList.add('class1');
+  container_element.appendChild(div1);
+  if (data2) {container_element.appendChild(div2)};
+}
+
+fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "815636d2f5msh5b6f6b41061625ep10f46fjsn4221a8a8f565"
+	}
+})
+.then(response => response.json().then(data => {
+  divAddData("nbSuceptible", "5,424,242");
+  divAddData("nbSick", data.total_cases, data.new_cases);
+  divAddData("nbDead", data.total_deaths, data.new_deaths);
+  divAddData("nbImmune", data.total_recovered);
+}))
+.catch(err => {
+    console.log(err);
+});
