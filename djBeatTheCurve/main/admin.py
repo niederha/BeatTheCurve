770fdb476261920b81e828f3dc8bed794a4cd7b7
Valentin Borgeaud <valentin.borgeaud@epfl.ch>
2020-04-06 13:50:17 +0200

    Add outing reason model to admin panel

--- djBeatTheCurve/main/admin.py
+++ djBeatTheCurve/main/admin.py
@@ -4,6 +4,7 @@ from .models import Commorbidity
 from .models import Symptom
 from .models import CustomUser
 from .models import GameInfo
+from .models import OutingReason
 
 # Register your models here.
 
@@ -11,3 +12,4 @@ admin.site.register(Commorbidity)
 admin.site.register(Symptom)
 admin.site.register(CustomUser)
 admin.site.register(GameInfo)
+admin.site.register(OutingReason)
