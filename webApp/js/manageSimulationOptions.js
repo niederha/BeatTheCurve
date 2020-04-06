8de1f6e01b25ac10b0bd3a73aa1c2f0d06e5f481
Loïc Niederhauser <loic.niederhauser@gmail.com>
2020-04-06 12:54:11 +0200

    Added sd-frac slider

--- webApp/js/manageSimulationOptions.js
+++ webApp/js/manageSimulationOptions.js
@@ -9,14 +9,20 @@ function sendSimulationData(){
             isolationOfInfected: false,
             goOutFrq: 7
         }
-    } 
+    }
+    sendSimulationData.simulationData.country;
     console.log(sendSimulationData.simulationData);
 }
 
 function initialiseParamView(){
     countrySelector.value = sendSimulationData.simulationData.country;
+    sliderSD.value = sendSimulationData.simulationData.fracFollowSD;
 }
 
+function sliderSDCB(){
+    sendSimulationData.simulationData.fracFollowSD = sliderSD.value;
+    textSD.innerHTML = "Fraction of pepople following social distancing: " + sliderSD.value + "%";
+}
 
 sendSimulationData(); // Get the first simulation
 
@@ -25,6 +31,9 @@ var countrySelector = document.getElementById("countrySelector");
 countrySelector.onchange = function(){
     sendSimulationData.simulationData.country = countrySelector.value};
 
+var sliderSD = document.getElementById("sliderSD");
+var textSD = document.getElementById("textSD")
+sliderSD.oninput = sliderSDCB
 
 var buttonSimulate = document.getElementById("buttonSimulate");
 buttonSimulate.onclick = sendSimulationData;
